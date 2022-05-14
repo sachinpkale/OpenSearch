1773777db04cecf01a1618a70c0630f9dc00903f
Sachin Kale <kalsac@amazon.com>
2022-05-14 22:25:30 +0530

    Upload segment to remote store post refresh

--- server/src/main/java/org/opensearch/index/shard/RemoteStoreRefreshListener.java
+++ server/src/main/java/org/opensearch/index/shard/RemoteStoreRefreshListener.java
@@ -8,10 +8,20 @@
 
 package org.opensearch.index.shard;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.lucene.search.ReferenceManager;
 import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.IOContext;
 
 import java.io.IOException;
+import java.nio.file.NoSuchFileException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 /**
  * RefreshListener implementation to upload newly created segment files to the remote store
@@ -20,10 +30,13 @@ public class RemoteStoreRefreshListener implements ReferenceManager.RefreshListe
 
     private final Directory storeDirectory;
     private final Directory remoteDirectory;
+    private final Map<String, String> filesUploadedToRemoteStore;
+    private static final Logger logger = LogManager.getLogger(RemoteStoreRefreshListener.class);
 
     public RemoteStoreRefreshListener(Directory storeDirectory, Directory remoteDirectory) {
         this.storeDirectory = storeDirectory;
         this.remoteDirectory = remoteDirectory;
+        this.filesUploadedToRemoteStore = new ConcurrentHashMap<>();
     }
 
     @Override
@@ -33,6 +46,31 @@ public class RemoteStoreRefreshListener implements ReferenceManager.RefreshListe
 
     @Override
     public void afterRefresh(boolean didRefresh) throws IOException {
-        // ToDo Add implementation
+        if (didRefresh) {
+            // ToDo:
+            //   1. Based on a config, run the following logic async
+            //   2. Instead of deleting files in sync manner, mark them and delete later in another thread.
+            Set<String> localFiles = Arrays.stream(storeDirectory.listAll()).collect(Collectors.toSet());
+            for (String file : localFiles) {
+                if (!filesUploadedToRemoteStore.containsKey(file)) {
+                    try {
+                        remoteDirectory.copyFrom(storeDirectory, file, file, IOContext.DEFAULT);
+                        filesUploadedToRemoteStore.put(file, file);
+                    } catch (NoSuchFileException e) {
+                        logger.info("The file {} does not exist anymore. It can happen in case of temp files", file);
+                    }
+                }
+            }
+            Set<String> remoteFilesToBeDeleted = new HashSet<>();
+            for (String file : filesUploadedToRemoteStore.keySet()) {
+                if (!localFiles.contains(file)) {
+                    remoteDirectory.deleteFile(file);
+                    remoteFilesToBeDeleted.add(file);
+                }
+            }
+            if (!remoteFilesToBeDeleted.isEmpty()) {
+                remoteFilesToBeDeleted.forEach(filesUploadedToRemoteStore::remove);
+            }
+        }
     }
 }
