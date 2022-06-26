dedd04e49301ff537164a8a609a9fbed9371dc7d
Sachin Kale <kalsac@amazon.com>
2022-06-26 11:46:07 +0530

    Pass supplier of RepositoriesService to RemoteDirectoryFactory
    Signed-off-by: Sachin Kale <kalsac@amazon.com>

--- server/src/main/java/org/opensearch/node/Node.java
+++ server/src/main/java/org/opensearch/node/Node.java
@@ -618,7 +618,7 @@ public class Node implements Closeable {
             rerouteServiceReference.set(rerouteService);
             clusterService.setRerouteService(rerouteService);
 
-            final RemoteDirectoryFactory remoteDirectoryFactory = new RemoteDirectoryFactory(repositoriesServiceReference.get());
+            final RemoteDirectoryFactory remoteDirectoryFactory = new RemoteDirectoryFactory(repositoriesServiceReference::get);
 
             final IndicesService indicesService = new IndicesService(
                 settings,
