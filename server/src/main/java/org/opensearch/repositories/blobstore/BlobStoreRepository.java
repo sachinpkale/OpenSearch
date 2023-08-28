8dd181a6208faa3316eca4ceb25cd84d17e4036b
Dharmesh 💤 <sdharms@amazon.com>
2023-08-28 14:33:41 +0530

    Addressed few comments, updated javadocs and added verify logic to be updated once system repo pr is merged
    Signed-off-by: Dharmesh 💤 <sdharms@amazon.com>

--- server/src/main/java/org/opensearch/repositories/blobstore/BlobStoreRepository.java
+++ server/src/main/java/org/opensearch/repositories/blobstore/BlobStoreRepository.java
@@ -3119,6 +3119,12 @@ public abstract class BlobStoreRepository extends AbstractLifecycleComponent imp
 
     @Override
     public void verify(String seed, DiscoveryNode localNode) {
+        /*
+        if(!isSystemRepository) {
+            assertSnapshotOrGenericThread();
+        }
+        Update the assertion method invocation with this once #9088 is merged.
+        */
         assertSnapshotOrGenericThread();
         if (isReadOnly()) {
             try {
