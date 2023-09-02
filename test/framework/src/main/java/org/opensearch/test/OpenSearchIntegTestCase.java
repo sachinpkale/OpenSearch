3e8475190849c42b7f52cbb3c1d8a158ebfe5986
Ashish Singh <ssashish@amazon.com>
2023-09-02 16:37:28 +0530

    Fix all tests in RemoveCorruptedShardDataCommandIT - 10 iters passed
    Signed-off-by: Ashish Singh <ssashish@amazon.com>

--- test/framework/src/main/java/org/opensearch/test/OpenSearchIntegTestCase.java
+++ test/framework/src/main/java/org/opensearch/test/OpenSearchIntegTestCase.java
@@ -2601,8 +2601,8 @@ public abstract class OpenSearchIntegTestCase extends OpenSearchTestCase {
     }
 
     protected boolean isIndexRemoteStoreEnabled(String index) throws Exception {
-        return client().admin().indices().getSettings(new GetSettingsRequest().indices("index")).get()
-            .getSetting("index", IndexMetadata.SETTING_REMOTE_STORE_ENABLED).equals(Boolean.TRUE.toString());
+        return client().admin().indices().getSettings(new GetSettingsRequest().indices(index)).get()
+            .getSetting(index, IndexMetadata.SETTING_REMOTE_STORE_ENABLED).equals(Boolean.TRUE.toString());
     }
 
 }
