bc5a6356724f8e4e7ab98c7f3486eb87d3bc4c16
Gaurav Bafna <gbbafna@amazon.com>
2023-09-02 20:47:25 +0530

    Adding isRemoteStoreEnabled and await fix relocation IT

--- server/src/internalClusterTest/java/org/opensearch/recovery/RelocationIT.java
+++ server/src/internalClusterTest/java/org/opensearch/recovery/RelocationIT.java
@@ -190,6 +190,7 @@ public class RelocationIT extends OpenSearchIntegTestCase {
         assertThat(client().prepareSearch("test").setSize(0).execute().actionGet().getHits().getTotalHits().value, equalTo(20L));
     }
 
+    @AwaitsFix(bugUrl = "SeqNoStats doesn't match for Remote Store, which is expected")
     public void testRelocationWhileIndexingRandom() throws Exception {
         int numberOfRelocations = scaledRandomIntBetween(1, rarely() ? 10 : 4);
         int numberOfReplicas = randomBoolean() ? 0 : 1;
@@ -769,6 +770,9 @@ public class RelocationIT extends OpenSearchIntegTestCase {
 
     private void assertActiveCopiesEstablishedPeerRecoveryRetentionLeases() throws Exception {
         assertBusy(() -> {
+            if (isRemoteStoreEnabled()) {
+                return;
+            }
             for (final String it : client().admin().cluster().prepareState().get().getState().metadata().indices().keySet()) {
                 Map<ShardId, List<ShardStats>> byShardId = Stream.of(client().admin().indices().prepareStats(it).get().getShards())
                     .collect(Collectors.groupingBy(l -> l.getShardRouting().shardId()));
